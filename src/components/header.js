e6da45671af5ec9d99709c8edf44beede3b98296
esmakybasi <koybasiesma19@gmail.com>
2023-10-02 03:10:57 +0300

    css update

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 // src/components/Header.js
 import React from 'react';
-import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, FormGroup } from 'react-bootstrap';
 
 const Header = ({ username }) => {
     return (
-        <Navbar bg="light" expand="lg" className="flex-shrink-0">
-            <Navbar.Brand href="#home" style={{marginLeft: '15px'}} className="fw-bold d-flex align-items-center">
+        <Navbar bg="light" expand="lg" className="flex-shrink-0" style={{ boxShadow: '0px 3px 6px #00000029' }}>
+            <Navbar.Brand href="#home" style={{marginLeft: '20px'}} className="fw-bold d-flex align-items-center">
                 {username}
                 <img src={process.env.PUBLIC_URL + '/img/iconh.svg'} alt="icon" style={{height: '20px', marginLeft: '10px'}} />
             </Navbar.Brand>
@@ -13,7 +13,10 @@ const Header = ({ username }) => {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto"></Nav>
                 <Form inline style={{marginRight: '15px'}} className="d-flex align-items-center">
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" style={{borderRadius: '20px'}} />
+                    <FormGroup style={{ position: 'relative' }}>
+                        <i className="fas fa-search" style={{ position: 'absolute', top: '10px', right: '10px', opacity: '0.5'}}></i>
+                        <FormControl type="text" className="mr-sm-2" style={{borderRadius: '20px', paddingLeft: '30px'}}  />
+                    </FormGroup>
                     <img src={process.env.PUBLIC_URL + '/img/avatar.svg'} alt="avatar" style={{height: '40px', borderRadius: '50%', marginLeft: '10px'}} />
                 </Form>
             </Navbar.Collapse>
--- src/components/userlist.js
+++ src/components/userlist.js
@@ -1,6 +1,6 @@
 // src/components/UserList.js
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Form } from 'react-bootstrap';
 import SummaryCard from './summaryCard';
 import FilterBar from './FilterBar';
 
@@ -16,8 +16,8 @@ const UserList = () => {
     const totalSales = 560000;
 
     return (
-        <Container style={{ paddingTop: "50px",width: '900px', height: '700px' }}>
-            <Row style={{ width: '900px',marginBottom: '20px' }}>
+        <Container style={{ paddingTop: "50px",width: '1000px', height: '700px' }}>
+            <Row style={{ width: '1000px',marginBottom: '20px' }}>
                 <SummaryCard title="Total Sales" value={`$${totalSales/1000}K`} style={{ flex: 1 }}/>
                 <SummaryCard title="Total Sales" value={`$${totalSales/1000}K`} style={{ flex: 1 }}/>
                 <SummaryCard title="Total Sales" value={`$${totalSales/1000}K`} style={{ flex: 1 }}/>
@@ -28,16 +28,22 @@ const UserList = () => {
 
             <div style={{ borderRadius:'7px', overflowX:'hidden', backgroundColor:'#fff'}}>
                 <div>
-                    <div style={{ display:'flex' , backgroundColor:'#F7F8FC'}}>
-                        <div style={{ flexBasis: '20%', padding: '0.75rem' }}>Learners</div>
+                    <div style={{ display:'flex' , backgroundColor:'#F7F8FC', height: '70px', alignItems: 'center'}}>
+                        <div style={{ flexBasis: '20%', padding: '0.75rem', display: 'flex', justifyContent: 'center' }}>
+                            <Form.Check style={{ marginRight: '10px' }} />
+                            Learners
+                        </div>
                         <div style={{ flexBasis: '20%', padding: '0.75rem' }}>Language</div>
                         <div style={{ flexBasis: '20%', padding: '0.75rem' }}>Occupation</div>
                         <div style={{ flexBasis: '20%', padding: '0.75rem' }}>Objective</div>
                         <div style={{ flexBasis: '20%', padding: '0.75rem' }}>Subscription</div>
                     </div>
                     {users.map(user => (
-                        <div key={user.id} style={{ display:'flex', borderBottom:'1px solid #dee2e6'}}>
-                            <div style={{ flexBasis: '20%', padding: '0.75rem' }}>{user.learners}</div>
+                        <div key={user.id} style={{ display:'flex', borderBottom:'1px solid #dee2e6', height: '80px', alignItems: 'center' }}>
+                            <div style={{ flexBasis: '20%', padding: '0.75rem', display: 'flex', justifyContent: 'center' }}>
+                                <Form.Check style={{ marginRight: '10px' }} />
+                                {user.learners}
+                            </div>
                             <div style={{ flexBasis: '20%', padding: '0.75rem' }}>{user.language}</div>
                             <div style={{ flexBasis: '20%', padding: '0.75rem' }}>{user.occupation}</div>
                             <div style={{ flexBasis: '20%', padding: '0.75rem' }}>{user.objective}</div>
